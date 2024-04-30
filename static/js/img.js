6e96b9d74cf58c61f4bdd778b4d20d1427b64907
Tiking <116478171+Tiking-010@users.noreply.github.com>
2024-04-30 13:06:56 +0800

    Delete static directory

--- static/js/img.js
+++
@@ -1,5 +0,0 @@
-  var mouse = document.querySelector('.mouse');
-        window.addEventListener('mousemove',function(event){    
-            mouse.style.left = event.clientX - mouse.offsetWidth/2 + 'px' ;
-            mouse.style.top = event.clientY -mouse.offsetHeight/2 + 'px';       
-        })// JavaScript Document
